5179cfcfbe74e242e220b7ab2193995299c12463
Magnu Windell Araújo Santos <magnuwindell@gmail.com>
2019-11-03 01:53:57 -0300

    feat: distribuicao binomial

---
+++ Estatistica I/distribuicao_binomial.r
@@ -0,0 +1,15 @@
+#distribuicao binomial
+
+dbinom(2, 5, 0.5)
+dbinom(3, 5, 0.5)
+dbinom(1, 5, 0.5)
+dbinom(0, 4, 0.25)
+dbinom(1, 4, 0.25)
+dbinom(2, 4, 0.25)
+dbinom(3, 4, 0.25)
+dbinom(4, 4, 0.25)
+
+pbinom(4, 4, 0.25)
+
+dbinom(7, 12, 0.25)
+pbinom(7, 12, 0.25)
